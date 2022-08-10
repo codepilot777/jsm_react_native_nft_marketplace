2dade5af12d0c70566bbf691408c323bf6453801
Lok Yiu Yeung <ryanyeung@Loks-MacBook-Air.local>
2022-08-10 18:23:21 +0800

    third commit

--- screens/details.js
+++ screens/details.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from 'react-native';
 
-const Details = () => {
+import { COLORS, SIZES,FONTS, SHADOWS, assets } from '../constants';
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from '../components/Button';
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
+  console.log(`${data} `)
   return (
     <dDv>Details</dDv>
   )
